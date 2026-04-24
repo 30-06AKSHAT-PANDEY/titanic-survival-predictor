7f5576308d2a0dd93478ea448cfdfb692876f57d
Akshat Pandey <137032734+30-06AKSHAT-PANDEY@users.noreply.github.com>
2026-04-24 15:47:14 +0530

    Update survived.py

--- survived.py
+++ survived.py
@@ -1,6 +1,13 @@
 import pandas as pd
-from sklearn.model_selection imort train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 df=pd.read_csv("train")
 X=df[["Sex","Pclass","Age","Fare"]].copy()
 X["Sex"]=X["Sex"].map({"male": 0, "female": 1})
-X["Age"]=X["Age"].fillna(mean("Age")
+X["Age"]=X["Age"].fillna(X["Age"].mean())
+Y=df["Survived"]
+X_train, X_test, Y_train, Y_test=train_test_split(X,Y,test_size=0.2)
+model=LogisticRegression(max_iter=1000)
+model.fit(X_train, Y_train)
+print("Accuracy:", model.score(X_test,Y_test))
+
